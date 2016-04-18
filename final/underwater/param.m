a7eea007e9ca431289b48b1eb781ef3a9545115b
Jae Lee <nagneeve@gmail.com>
2016-04-18 06:43:53 -0600

    all controllers are working for final

--- final/underwater/param.m
+++ final/underwater/param.m
@@ -1,76 +1,112 @@
 clear; close all; clc;
 
 % actual system parameters
-    AP.m     = 10;   % kg
-    AP.mu1   = 25;   % kg/m
-    AP.mu2   = 15;   % kg s^2 / m^3
-    AP.alpha = 0.8;  % unitless
-    AP.vc    = 0.5;  % m/s
-    P.Tmax   = 50;     % N
-    
+AP.m     = 10;   % kg
+AP.mu1   = 25;   % kg/m
+AP.mu2   = 15;   % kg s^2 / m^3
+AP.alpha = 0.8;  % unitless
+AP.vc    = 0.5;  % m/s
+P.Tmax   = 50;     % N
+
 
 % initial conditions
-    AP.p0 = 0; % m
-    AP.v0 = 0; % m/s
+AP.p0 = 0; % m
+AP.v0 = 0; % m/s
 
 % parameters known to the controller
-    % size of uncertainty in parameters
-    S = 0.0;  % up to 100*S % change in parameters
-    P.m     = AP.m*(1+2*S*rand-S);      % kg
-    P.mu1   = AP.mu1*(1+2*S*rand-S);    % kg/m
-    P.mu2   = AP.mu2*(1+2*S*rand-S);    % kg s^2 / m^3
-    P.alpha = AP.alpha*(1+2*S*rand-S);  % unitless
-    P.vc    = AP.vc*(1+2*S*rand-S);     % m/s
+% size of uncertainty in parameters
+S = 0.0;  % up to 100*S % change in parameters
+P.m     = AP.m*(1+2*S*rand-S);      % kg
+P.mu1   = AP.mu1*(1+2*S*rand-S);    % kg/m
+P.mu2   = AP.mu2*(1+2*S*rand-S);    % kg s^2 / m^3
+P.alpha = AP.alpha*(1+2*S*rand-S);  % unitless
+P.vc    = AP.vc*(1+2*S*rand-S);     % m/s
 
 % sample rate for controller
-    P.Ts = 0.01;
+P.Ts = 0.01;
 % parameter for dirty derivative
-    P.tau = 0.05;
+P.tau = 0.05;
 
 % select which controller to use in simulation
-    P.control_selection_flag = 5;  % 1==PD, 2==backstepping, 3==feedback linearization, 4==sliding mode, 5==adaptive
-    P.num_adaptive_param = 1;  % one except for option 5
-    
+P.control_selection_flag = 5;  % 1==PD, 2==backstepping, 3==feedback linearization, 4==sliding mode, 5==adaptive
+P.num_adaptive_param = 1;  % one except for option 5
+
 % parameters for each controller
-    switch P.control_selection_flag,
-        case 1, % PD control
-            P.kp = 10;
-            P.kd = 10;
-            AP.vc = 0;      % vc=0 in the problem
-        case 2, % backstepping
-            P.k1 = 10;      % tuning parameter
-            P.k2 = 10;      % tuning parameter
-%             P.m     = AP.m;      % kg
-%             P.mu1   = AP.mu1;    % kg/m
-%             P.mu2   = AP.mu2;    % kg s^2 / m^3
-%             P.alpha = AP.alpha;  % unitless
-%             P.vc    = AP.vc;     % m/s
-            
-        case 3, % feedback linearization
-            P.k1 = 10;
-            P.k2 = 10;
-        case 4, % sliding mode
-            P.ke = 5;
-            P.beta = 1;
-        case 5, % adaptive
-            % baseline controller
-            P.A = [0 1 0; 0 2*P.mu1*P.vc/P.m+4*P.mu2*P.vc^3/P.m 0; 1 0 0];
-            P.B = [0; P.alpha/P.m; 0];
-            P.B_ref = [0; 0; -1];
-            lqr_Q = 1000000*eye(3);
-            lqr_R = eye(1);
-            
-            [K,S,E] = lqr(P.A, P.B, lqr_Q, lqr_R);
-            P.K = K;
-            
-            % adaptive gains
-%             P.Gam = diag([]);
-%             P.num_adaptive_param = size(P.Gam,1);
-
-    end
-
-
+switch P.control_selection_flag,
+    case 1, % PD control
+        P.kp = 10;
+        P.kd = 10;
+        AP.vc = 0;      % vc=0 in the problem
+    case 2, % backstepping
+        P.k1 = 10;      % tuning parameter
+        P.k2 = 10;      % tuning parameter
+        %             P.m     = AP.m;      % kg
+        %             P.mu1   = AP.mu1;    % kg/m
+        %             P.mu2   = AP.mu2;    % kg s^2 / m^3
+        %             P.alpha = AP.alpha;  % unitless
+        %             P.vc    = AP.vc;     % m/s
         
-    
+    case 3, % feedback linearization
+        P.k1 = 10;
+        P.k2 = 10;
+    case 4, % sliding mode
+        P.ke = 5;
+        P.beta = 1;
+    case 5, % adaptive
+        % baseline controller
+        P.A = [0 1 0; 0 2*P.mu1*P.vc/P.m+4*P.mu2*P.vc^3/P.m 0; 1 0 0];
+        P.B = [0; P.alpha/P.m; 0];
+        P.B_ref = [0; 0; -1];
+        %             lqr_Q = 1000000*eye(3);
+        %             lqr_R = eye(1);
+        lqr_Q = diag([...
+            100;...     % penelaty on p
+            100;...     % penalty on v
+            10;...     % penalty on integrator
+            ]);
+        lqr_R = 1/P.Tmax;   % penalty on T
+        
+        [K,S,E] = lqr(P.A, P.B, lqr_Q, lqr_R);
+        P.K = K;
+                
+        % adaptive gains
+        P.Gam = 10*diag([...
+            1;...  % baseline controller
+            1;...  % constant
+            1;...  % p
+            1;...  % v
+            1;...  % v+vc
+            1;...  % -mu1*v*abs(v)/m
+            1;...  % -mu2*v*abs(v)^3/m
+            1;...  % alpha/m
+            ]);
+        P.num_adaptive_param = size(P.Gam,1);
+        P.deadzone_limit = [...
+            0; %2*pi/180;... % limit on p error
+            0; %0.05;...     % limit on v error
+            0; %0.1;...        % limit on integral error
+            ];
+        P.proj_limit = 2*[...
+            1;...  % baseline controller
+            1;...  % constant
+            1;...  % p
+            1;...  % v
+            1;...  % v+vc
+            1;...  % -mu1*v*abs(v)/m
+            1;...  % -mu2*v*abs(v)^3/m
+            1;...  % alpha/m
+            ];
+        % observer in the reference model
+        P.A_ref = P.A - P.B*P.K;
+        P.C_ref = [1 0 0];
+        nu = 0.01;
+        [L_nu, S_nu] = lqr(P.A_ref', eye(3), (nu+1)/nu*eye(3), nu/(nu+1)*eye(3));
+        P.L_ref_obsv = L_nu';
+        P.P_ref_obsv = inv(S_nu);
+end
+
+
+
+
 
 
--- final/underwater/underwater_ctrl.m
+++ final/underwater/underwater_ctrl.m
@@ -1,136 +1,166 @@
 function out=underwater_ctrl(in,P)
-    pr    = in(1);
-    p     = in(2);
-    v     = in(3);
-    t     = in(4);
+pr    = in(1);
+p     = in(2);
+v     = in(3);
+t     = in(4);
 
-    persistent thetahat;
-    persistent differentiator;
-    persistent error_d1;
-    persistent integrator;
-    
-    switch P.control_selection_flag,
-        case 1, % PD control
-            p_ref = pr;
-            error = p_ref-p;
-            if t==0
-                differentiator = 0;
-                error_d1 = 0;
-            else
-                differentiator = (2*P.tau-P.Ts)/(2*P.tau+P.Ts)*differentiator...
-                                 + 2/(2*P.tau+P.Ts)*(error-error_d1);
-            end
-            error_d1 = error;
-            
-            T = P.kp*error + P.kd*differentiator;
-            
-            thetahat = zeros(P.num_adaptive_param,1);           
-            
-        case 2, % backstepping
-            p_ref = pr;
-            z = p - p_ref;
-            xi = -P.vc - P.k1*z;
-            xi_dot = -P.k1*(v + P.vc);
-            
-            T = P.m/P.alpha*(P.mu1*v*abs(v)/P.m + P.mu2*v*abs(v)^3/P.m + xi_dot - z- P.k2*(v - xi));            
-            
+persistent thetahat;
+persistent differentiator;
+persistent error_d1;
+persistent integrator;
+persistent xref
+
+switch P.control_selection_flag,
+    case 1, % PD control
+        p_ref = pr;
+        error = p_ref-p;
+        if t==0
+            differentiator = 0;
+            error_d1 = 0;
+        else
+            differentiator = (2*P.tau-P.Ts)/(2*P.tau+P.Ts)*differentiator...
+                + 2/(2*P.tau+P.Ts)*(error-error_d1);
+        end
+        error_d1 = error;
+        
+        T = P.kp*error + P.kd*differentiator;
+        
+        thetahat = zeros(P.num_adaptive_param,1);
+        
+    case 2, % backstepping
+        p_ref = pr;
+        z = p - p_ref;
+        xi = -P.vc - P.k1*z;
+        xi_dot = -P.k1*(v + P.vc);
+        
+        T = P.m/P.alpha*(P.mu1*v*abs(v)/P.m + P.mu2*v*abs(v)^3/P.m + xi_dot - z- P.k2*(v - xi));
+        
+        thetahat = zeros(P.num_adaptive_param,1);
+        
+    case 3, % feedback linearization
+        p_ref = pr;
+        z1 = p - p_ref;
+        z2 = v + P.vc;
+        
+        nu = -P.k1*z1 - P.k2*z2;
+        
+        T = P.m/P.alpha * (P.mu1*v*abs(v)/P.m + P.mu2*v*abs(v)^3/P.m + nu);
+        
+        thetahat = zeros(P.num_adaptive_param,1);
+        
+    case 4, % sliding mode
+        p_ref = pr;
+        error = p - p_ref;
+        if t==0
+            differentiator = 0;
+            error_d1 = 0;
+        else
+            differentiator = (2*P.tau-P.Ts)/(2*P.tau+P.Ts)*differentiator...
+                + 2/(2*P.tau+P.Ts)*(error-error_d1);
+        end
+        error_d1 = error;
+        s = differentiator + P.ke*error;
+        p_x = abs(-50*v*abs(v)/0.1 - 15*v*abs(v)^3/0.1)+abs(20*(v+1));
+        
+        T = -(p_x + P.beta)*sat1(s/0.1);
+        %             T = -(p_x + P.beta)*sign(s);
+        
+        thetahat = zeros(P.num_adaptive_param,1);
+        
+    case 5, % adaptive
+        % baseline controller
+        p_ref = pr;
+        error = p - p_ref;
+        if t==0
+            integrator = 0;
+            error_d1 = 0;
+        else
+            integrator = integrator + (P.Ts/2)*(error+error_d1);
+        end
+        error_d1 = error;
+        x = [p; v; integrator];
+        u_bl = -P.K*x;
+        
+        
+        % build regressor vector
+        Phi = [...
+            u_bl;...  % baseline controller
+            1;...
+            p;...
+            v;...
+            v+P.vc;...
+            -P.mu1*v*abs(v)/P.m;...
+            -P.mu2*v*abs(v)^3/P.m;...
+            P.alpha/P.m;...
+            ];
+        % initialize persistent variables
+        if t<P.Ts,
+            xref = zeros(3,1);
             thetahat = zeros(P.num_adaptive_param,1);
-            
-        case 3, % feedback linearization
-            p_ref = pr;
-            z1 = p - p_ref;
-            z2 = v + P.vc;
-            
-            nu = -P.k1*z1 - P.k2*z2;
-
-            T = P.m/P.alpha * (P.mu1*v*abs(v)/P.m + P.mu2*v*abs(v)^3/P.m + nu);
-            
-            thetahat = zeros(P.num_adaptive_param,1);
-
-        case 4, % sliding mode
-            p_ref = pr;
-            error = p - p_ref;
-            if t==0
-                differentiator = 0;
-                error_d1 = 0;
-            else
-                differentiator = (2*P.tau-P.Ts)/(2*P.tau+P.Ts)*differentiator...
-                                 + 2/(2*P.tau+P.Ts)*(error-error_d1);
-            end
-            error_d1 = error;
-            s = differentiator + P.ke*error;
-            p_x = abs(-50*v*abs(v)/0.1 - 15*v*abs(v)^3/0.1)+abs(20*(v+1));
-            
-            T = -(p_x + P.beta)*sat1(s/0.1);
-%             T = -(p_x + P.beta)*sign(s);            
-
-            thetahat = zeros(P.num_adaptive_param,1);
-            
-        case 5, % adaptive
-            % baseline controller
-            p_ref = pr;
-            error = p - p_ref;            
-            if t==0
-                integrator = 0;
-                error_d1 = 0;
-            else
-                integrator = integrator + (P.Ts/2)*(error+error_d1);
-            end
-            error_d1 = error;
-            x = [p; v; integrator];
-            u_bl = -P.K*x;
-
-            T = u_bl;
-            thetahat = zeros(P.num_adaptive_param,1);
-            
-    end
-    out = [T; p_ref; thetahat];
+        end
+        % update reference model and adaptive parameters
+        N = 10;
+        for i=1:N,
+            xref = xref + P.Ts/N*(P.A_ref*xref + P.B_ref*pr + P.L_ref_obsv*(x-xref));
+            zref = P.C_ref*xref;
+            e = deadzone(x-xref, P.deadzone_limit);
+            thetahat = sat( thetahat + P.Ts/N*Proj(thetahat,P.Gam*Phi*e'*P.P_ref_obsv*P.B,P), P.proj_limit);
+            u_ad = -thetahat'*Phi;
+        end
+%         u_ad = 0;
+        
+        u = u_bl + u_ad;
+        
+        T = u;
+        
+end
+out = [T; p_ref; thetahat];
 end
 
 
 %-----------------------------------------------------------------
 % deadzone operator
 function out = deadzone(in,limit)
-    out = in;
-    for i=1:length(in),
-        if abs(in(i))<=limit(i),
-            out(i) = 0;
-        end
+out = in;
+for i=1:length(in),
+    if abs(in(i))<=limit(i),
+        out(i) = 0;
     end
+end
 end
 
 %-----------------------------------------------------------------
 % projection operator
 function yout = Proj(theta,y,P)
-    yout = y;
-    delta = 0.1;
-    for i=1:length(theta),
-        if (theta(i)>(P.proj_limit(i)-delta)) & (y(i)>0)
-            yout(i) = ((P.proj_limit(i)-theta(i))/delta)*y(i);
-        end
-        if (theta(i)<(-P.proj_limit(i)+delta)) & (y(i)<0),
-            yout(i) = ((theta(i)-P.proj_limit(i))/delta)*y(i);
-        end
+yout = y;
+delta = 0.1;
+for i=1:length(theta),
+    if (theta(i)>(P.proj_limit(i)-delta)) & (y(i)>0)
+        yout(i) = ((P.proj_limit(i)-theta(i))/delta)*y(i);
     end
+    if (theta(i)<(-P.proj_limit(i)+delta)) & (y(i)<0),
+        yout(i) = ((theta(i)-P.proj_limit(i))/delta)*y(i);
+    end
+end
 end
 
 
 %-----------------------------------------------------------------
 % saturation function
 function out = sat(in,limit)
-    out = in;
-    for i=1:length(limit),
-        if     in(i) > limit(i),      out(i) = limit(i);
-        elseif in(i) < -limit(i),     out(i) = -limit(i);
-        end
+out = in;
+for i=1:length(limit),
+    if     in(i) > limit(i),      out(i) = limit(i);
+    elseif in(i) < -limit(i),     out(i) = -limit(i);
     end
+end
 end
 
 %-----------------------------------------------------------------
 % saturation function
 function out = sat1(in)
-    out = in;
-    if     in > 1,      out = 1;
-    elseif in < -1,     out = -1;
-    end
- end
+out = in;
+if     in > 1,      out = 1;
+elseif in < -1,     out = -1;
+end
+end
